a6a5cc66ea5a1f2c1623aafaf230924f252c2f16
rakhaviantoni <halo@rakhaviantoni.com>
2021-06-21 00:09:46 +0700

    responsive fix

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -29,8 +29,8 @@ const Home = () => {
       .d-flex
         div(className=css.Home)
           nav(className=css.Home__Navbar).row.sticky-top
-            .col-8
-            .col-4(className=css.Home__NavbarRight)
+            .col-lg-8
+            .col-lg-4(className=css.Home__NavbarRight)
               div(className=css.Home__NavbarSearch)
                 input(
                   type="text"
@@ -45,15 +45,15 @@ const Home = () => {
           .col-12(className=css.Home__Content)
             .container-fluid(style={padding:"0"})
               div(className=css.Home).col-12
-              if medias.filter(media => search.length > 0 ? media.name.toLowerCase().includes(search) : media).length > 0
-                ResponsiveMasonry(
-                  columnsCountBreakPoints={350: 2, 750: 3, 900: 6}).row
-                  Masonry(gutter="14px")
-                    = mediasContent
-              else
-                div(className=css.Home__Null_Content)
-                  img(src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-2506.jpg?size=626&ext=jpg")
-                  h1 No media found
+                if medias.filter(media => search.length > 0 ? media.name.toLowerCase().includes(search) : media).length > 0
+                  ResponsiveMasonry(
+                    columnsCountBreakPoints={350: 3, 750: 4, 900: 6}).row
+                    Masonry(gutter="14px")
+                      = mediasContent
+                else
+                  div(className=css.Home__Null_Content)
+                    img(src="https://img.freepik.com/free-vector/no-data-concept-illustration_114360-2506.jpg?size=626&ext=jpg")
+                    h1 No media found
     `
   )
 }
